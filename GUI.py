e1097933c3958eb3dc458e1ba82db6fd78353172
Todd Gerdy <toadimous@gmail.com>
2023-11-15 16:32:24 -0500

    forgot to commit recently

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from math import sqrt, cos, sin, radians
+from math import cos, sin, radians
 from tkinter import *
 from tkinter import ttk
-
+from scipy.interpolate import RegularGridInterpolator
 from Character import Character
 
 
@@ -13,6 +13,8 @@ class Gui(Tk):
         self.character_stats = tk.Frame(self)
         self.character_stats.pack(side='right')
         self.main_label = tk.Label(self.character_stats, text='Hello')
+        self.canvas_width = 400
+        self.canvas_height = 300
 
         # Setup Notebook
         self.notebook = ttk.Notebook(self)
@@ -41,58 +43,74 @@ class Gui(Tk):
         self.inventory.grid(column=0,
                             row=1,)
 
-
-# ----- Map Tab -----
+        # ----- Map Tab -----
         self.hex_map_canvas = Canvas(self.map_tab,
-                                     width=400,
-                                     height=300,
+                                     width=self.canvas_width,
+                                     height=self.canvas_height,
                                      bg='#000000')
         self.hex_map_canvas.pack()
 
         self.hexagons: list = []
-        self.init_grid(10, 7, 30, debug=True)
+        self.starting_hex(x=int(self.canvas_width / 2),
+                          y=int(self.canvas_height / 2),
+                          size=30,)
+        # self.init_grid(10, 7, 30, debug=True)
         self.hex_map_canvas.bind("<Button-1>", self.click)
 
-# ----- Dungeon Tab -----
+        # ----- Dungeon Tab -----
         # TODO: Create Dungeon logic
         ttk.Label(self.dungeon_tab,
                   text="Dungeon").grid()
 
-# ----- Settings Page -----
+        # ----- Settings Page -----
         # TODO: Create Settings Page Logic
         ttk.Label(self.settings_tab,
                   text="Settings Page").grid()
 
-    def init_grid(self, cols: int, rows: int, size: int, debug: bool):
-        """
-        2d grid of hexagons
-        :param cols:
-        :param rows:
-        :param size:
-        :param debug:
-        :return:
-        """
-        # TODO: Why won't it select the proper hex.
-
-        for c in range(cols):
-            if c % 2 == 0:
-                offset = size * sqrt(3) / 2
-            else:
-                offset = 0
-            for r in range(rows):
-                h = FillHexagon(self.hex_map_canvas,
-                                c * (size * 1.5),
-                                (r * (size * sqrt(3))) + offset,
-                                size,
-                                "#a1e2a1",
-                                f"{r}.{c}")
-                self.hexagons.append(h)
-
-                if debug:
-                    coords = f"{r}, {c}"
-                    self.hex_map_canvas.create_text(c * (size * 1.5) + (size / 2),
-                                                    (r * (size * sqrt(3))) + offset + (size / 2),
-                                                    text=coords)
+    def starting_hex(self, x: int, y: int, size: int):
+        hexagon = FillHexagon(self.hex_map_canvas,
+                              x=x,
+                              y=y,
+                              length=size,
+                              color="#a1e2a1",
+                              tags=f"{'help'}.{'me'}"
+                              )
+        # TODO: make actual coords
+        coords = f"{0}, {0}"
+        self.hex_map_canvas.create_text(x + (size * .5),
+                                        y + (size * .5),
+                                        text=coords)
+        self.hexagons.append(hexagon)
+        # print([f'{hex_coords.x}, {hex_coords.y}' for hex_coords in self.hexagons])
+        # def init_grid(self, cols: int, rows: int, size: int, debug: bool):
+        #     """
+        #     2d grid of hexagons
+        #     :param cols:
+        #     :param rows:
+        #     :param size:
+        #     :param debug:
+        #     :return:
+        #     """
+        #     # TODO: Why won't it select the proper hex.
+        #
+        #     for c in range(cols):
+        #         if c % 2 == 0:
+        #             offset = size * sqrt(3) / 2
+        #         else:
+        #             offset = 0
+        #         for r in range(rows):
+        #             h = FillHexagon(self.hex_map_canvas,
+        #                             c * (size * 1.5),
+        #                             (r * (size * sqrt(3))) + offset,
+        #                             size,
+        #                             "#a1e2a1",
+        #                             f"{r}.{c}")
+    #
+    #             if debug:
+    #                 coords = f"{r}, {c}"
+    #                 self.hex_map_canvas.create_text(c * (size * 1.5) + (size / 2),
+    #                                                 (r * (size * sqrt(3))) + offset + (size / 2),
+    #                                                 text=coords)
 
     def click(self, evt):
         """
@@ -102,6 +120,15 @@ class Gui(Tk):
         """
         x, y = evt.x, evt.y
         print(x, y)
+        center_coords = [hexagons.center_coords for hexagons in self.hexagons][0]
+        print(center_coords)
+        #TODO: How do I find the closest set of coordinates to the clicked space?  Maybe https://stackoverflow.com/questions/28036812/indexerror-too-many-indices-for-array
+
+        # itp = RegularGridInterpolator(
+        #     (evt.x, evt.y),
+        #     center_coords,
+        #     method='nearest')
+        # print(itp.values())
         for i in self.hexagons:
             i.selected = False
             i.isNeighbour = False
@@ -122,13 +149,16 @@ class FillHexagon:
         self.y = y  # top left y
         self.length = length  # length of a side
         self.color = color  # fill color
-
+        self.center_coords = []
         self.selected = False
         self.tags = tags
+        self.corners = []
 
         self.draw()
 
     def draw(self):
+        center_x: float
+        center_y: float
         start_x = self.x
         start_y = self.y
         coords: list = []
@@ -140,6 +170,14 @@ class FillHexagon:
             # print(coords)
             start_y = end_y
             start_x = end_x
+        center_x = (coords[3][0] + coords[0][0]) / 2
+        center_y = (coords[3][1] - coords[0][1]) / 2
+        self.center_coords.append(center_x)
+        self.center_coords.append(center_y)
+        for x, y in coords:
+            self.corners.append((x, y))
+        print(self.corners)
+        print(self.center_coords)
         self.parent.create_polygon(coords[0][0],
                                    coords[0][1],
                                    coords[1][0],
---
+++ README.md
@@ -0,0 +1,4 @@
+This is the readme for this:
+
+Potential websites to help.
+https://www.mapeditor.org/
